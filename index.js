67c5741ab5b88214bfa2b95148cc3a9f93f30a0b
Satish Marisetty <satishmarisetti79@gmail.com>
2024-04-12 11:27:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+let items = [];
+
+app.use(express.static(__dirname+'/frontend/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req,res){
+    res.render('index',{items: items});
+});
+
+app.post('/', function(req,res){
+    items.push(req.body.newItem);
+    res.redirect('/');
+});
+
+app.listen(3000,function(){
+    console.log('server is running');
+});
